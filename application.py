4dff8c143e2b2bfcf02b9dd5215e1819b639e6ff
Nghia Tran <thnghia0401@gmail.com>
2020-05-30 14:42:28 -0600

    fix x2

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ import datetime
 
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from tempfile import mkdtemp
